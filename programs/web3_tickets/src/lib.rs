7c92a02871b899c9dc7f703e3cd5a6ddf9e1f933
modjadji13 <modjadjimokgapa@gmail.com>
2026-05-09 23:40:49 +0200

    chore: sync ticketing program declare id

--- programs/web3_tickets/src/lib.rs
+++ programs/web3_tickets/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::system_program;
 
-declare_id!("11111111111111111111111111111111");
+declare_id!("35wzuQvuh6PkqoTe8sgZu8hx8cV4sG2G8h89zELaLmKD");
 
 #[program]
 pub mod web3_tickets {
