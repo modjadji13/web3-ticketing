82dce476437f059419950a83b3f2efb92459c246
modjadji13 <modjadjimokgapa@gmail.com>
2026-05-09 14:48:08 +0200

    docs: comment React entry point

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './style.css';
 
+// Vite loads this file from index.html. It creates the React root and renders
+// the app component into the <div id="root"></div> element.
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
